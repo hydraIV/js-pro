94923ffc36aadee50d3834c5e8b03c6373c67b62
Anton Petukhov <anton9981@gmail.com>
2019-05-31 21:28:36 +0300

    Update

--- project/js/main.js
+++ project/js/main.js
@@ -1,12 +1,13 @@
 const products = [
-    {id: 1, title: 'Notebook', price: 2000},
-    {id: 2, title: 'Mouse', price: 30},
-    {id: 3, title: 'Keyboard', price: 55},
-    {id: 4, title: 'Gamepad', price: 75},
+    {id: 1, title: 'Notebook', price: 2000, tumbnail: 'https://items.s1.citilink.ru/484768_v04_b.jpg'},
+    {id: 2, title: 'Mouse', price: 30, tumbnail: 'https://items.s1.citilink.ru/339095_v01_b.jpg'},
+    {id: 3, title: 'Keyboard', price: 55, tumbnail: 'https://items.s1.citilink.ru/339097_v01_b.jpg'},
+    {id: 4, title: 'Gamepad', price: 75, tumbnail: 'https://items.s1.citilink.ru/396400_v01_b.jpg'},
 ];
 
-const renderProduct = (title, price) => {
+const renderProduct = (tumbnail, title, price) => {
     return `<div class="product-item">
+                <img src="${tumbnail}"></img>
                 <h3>${title}</h3>
                 <p>${price}</p>
                 <button class="buy-btn">Купить</button>
@@ -14,7 +15,7 @@ const renderProduct = (title, price) => {
 };
 
 const renderPage = list => {
-    const productsList = list.map(item => renderProduct(item.title, item.price));
+    const productsList = list.map(item => renderProduct(item.tumbnail, item.title, item.price)).join(''); // map возвращает массив, нужно "склеить" все элементы в строку
     document.querySelector('.products').innerHTML = productsList;
 };
 
