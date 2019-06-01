457c4720a9ae961f5464785c4c4544e5c11ae387
Anton Petukhov <39304158+hydraIV@users.noreply.github.com>
2019-06-01 08:53:57 +0300

    Done

--- project/js/main.js
+++ project/js/main.js
@@ -5,17 +5,19 @@ const products = [
     {id: 4, title: 'Gamepad', price: 75, tumbnail: 'https://items.s1.citilink.ru/396400_v01_b.jpg'},
 ];
 
-const renderProduct = (tumbnail, title, price) => {
-    return `<div class="product-item">
-                <img src="${tumbnail}"></img>
-                <h3>${title}</h3>
-                <p>${price}&nbsp;$</p>
-                <div class="buy-btn">Купить</div>
-            </div>`
-};
+const renderProduct = (tumbnail, title, price) =>
+    `<div class="product-item">
+        <img src="${tumbnail}"></img>
+        <h3>${title}</h3>
+        <p>${price}&nbsp;$</p>
+        <div class="buy-btn">Купить</div>
+    </div>`;
 
 const renderPage = list => {
-    const productsList = list.map(item => renderProduct(item.tumbnail, item.title, item.price)).join(''); // map возвращает массив, нужно "склеить" все элементы в строку
+    const productsList = list.map(item => renderProduct(item.tumbnail, item.title, item.price)).join(''); 
+    // map возвращает массив товаров, а нужен обычный HTML. Необходимо "склеить" все элементы массива в строку
+    // В renderProduct убрал фигурный скобки, т.к. она сразу возвращает результат
+    // Значение по умолчанию можно передать, присвоив одному из аргументов это значение в конце (item.price = 100), но здесь это не нужно
     document.querySelector('.products').innerHTML = productsList;
 };
 
