0a42fcbcfbe044c8e4bdbc57de59ceb72dcd1a97
Anton Petukhov <39304158+hydraIV@users.noreply.github.com>
2019-06-08 17:10:42 +0300

    Update

--- project/js/main.js
+++ project/js/main.js
@@ -70,30 +70,3 @@ class Cart {
 }
 
 const products = new ProductsList();
-
-// const products = [
-//     {id: 1, title: 'Notebook', price: 2000},
-//     {id: 2, title: 'Mouse', price: 30},
-//     {id: 3, title: 'Keyboard', price: 55},
-//     {id: 4, title: 'Gamepad', price: 65},
-// ];
-//
-// const renderProduct = (title, price, img = `https://placehold.it/200x150`) => {
-//     return `<div class="product-item">
-//                  <img src="${img}" alt="${title}">
-//                  <div class="desc">
-//                      <h3>${title}</h3>
-//                      <p>${price}</p>
-//                      <button class="buy-btn">Купить</button>
-//                  </div>
-//              </div>`
-// };
-//
-// const renderPage = list => {
-//     // document.querySelector('.products').innerHTML = list.map(item => renderProduct(item.title, item.price)).join('');
-//     for (let product of list){
-//         document.querySelector('.products').insertAdjacentHTML('beforeend', renderProduct(product.title, product.price));
-//     }
-// };
-//
-// renderPage(products);
